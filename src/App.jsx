bd56eff6a32b37f0f1d12e5a81bc4f66303b5783
Rinki Bai <rinkirinki4349@gmail.com>
2025-04-12 11:44:56 +0530

    Updated App.jsx or index.html with necessary changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
 // Pages
 import Home from "./pages/Home";
 import Login from "./pages/Login";
